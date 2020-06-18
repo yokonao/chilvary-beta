573ecbbafdeafeaef80ffd30867526e227ed773a
yokonao <yokotukanao@me.com>
2020-06-18 09:02:40 +0900

    Fix sever problem

--- lib/crawler.tsx
+++ lib/crawler.tsx
@@ -3,20 +3,19 @@ import { fetchDirectoryContents } from "lib/s3";
 import { FileSystemCredentials } from "aws-sdk";
 
 async function listAllFilesAndDirectories() {
-  const directoryArrayPaths: string[][] = [];
-  const fileArrayPaths: string[][] = [];
-  const queue: string[][] = [[""]]; //corresponding to root directory
+  let directoryArrayPaths: string[][] = [];
+  let fileArrayPaths: string[][] = [];
+  let queue: string[][] = [[""]]; //corresponding to root directory
 
   while (queue.length != 0) {
     const searchDirectory = queue.shift();
     const posixPath = toPosix(searchDirectory, "/");
     const directoryContents = await fetchDirectoryContents(posixPath);
 
-    queue.concat(directoryContents.directories);
-    directoryArrayPaths.concat(directoryContents.directories);
-    fileArrayPaths.concat(directoryContents.files);
+    queue = [...queue, ...directoryContents.directories]
+    directoryArrayPaths = [...directoryArrayPaths, ...directoryContents.directories]
+    fileArrayPaths = [...fileArrayPaths, ...directoryContents.files]
   }
-
   return {
     files: fileArrayPaths,
     directories: directoryArrayPaths,
